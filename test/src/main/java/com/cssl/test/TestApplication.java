b1c0a4b8083c1e419b69d9f671b741d743d0c580
zeheng <2590757239@qq.com>
2020-10-23 15:13:35 +0800

    第一次

---
+++ test/src/main/java/com/cssl/test/TestApplication.java
@@ -0,0 +1,16 @@
+package com.cssl.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApplication {
+
+    public static void main(String[] args) {
+        System.out.println("java");
+        System.out.println("java");
+        System.out.println("java");
+        SpringApplication.run(TestApplication.class, args);
+    }
+
+}
---
+++ test/src/test/java/com/cssl/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cssl.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
