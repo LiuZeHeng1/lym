0b2e453e922d3aa3ed1c2741c497361489bfa4f9
zeheng <2590757239@qq.com>
2020-10-23 15:37:01 +0800

    4

--- test/src/main/java/com/cssl/pojo/Users.java
+++ test/src/main/java/com/cssl/pojo/Users.java
@@ -5,4 +5,12 @@ public class Users {
     private Integer id;
     private String name;
     private String sex;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
 }
